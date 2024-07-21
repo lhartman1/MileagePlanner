39ee43b649aeb9fb92c7d21984c963821ef10811
Luke <lhartm0@gmail.com>
2024-07-20 21:19:41 -0400

    Create MileageSlider

---
+++ app/src/main/java/com/example/mileageplanner/ui/MileageSlider.kt
@@ -0,0 +1,74 @@
+package com.example.mileageplanner.ui
+
+import androidx.annotation.IntRange
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
+@Preview(showBackground = true)
+@Composable
+private fun MileageSliderPreview() {
+    MileageSlider(sliderMax = 10)
+}
+
+@Composable
+fun MileageSlider(
+    @IntRange(from = 1) sliderMax: Int,
+    modifier: Modifier = Modifier,
+) {
+    val steps = sliderMax - 1
+    var value by rememberSaveable { mutableIntStateOf(0) }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "$value")
+        Slider(
+            value = value.toFloat(),
+            onValueChange = { value = it.roundToInt() },
+            valueRange = 0.0f..sliderMax.toFloat(),
+            steps = steps,
+            colors = SliderDefaults.colors().copy(
+                activeTickColor = Color.Transparent,
+                inactiveTickColor = Color.Transparent,
+            ),
+            modifier = Modifier
+                .graphicsLayer {
+                    rotationZ = 270f
+                    transformOrigin = TransformOrigin(0f, 0f)
+                }
+                .layout { measurable, constraints ->
+                    val placeable = measurable.measure(
+                        Constraints(
+                            minWidth = constraints.minHeight,
+                            maxWidth = constraints.maxHeight,
+                            minHeight = constraints.minWidth,
+                            maxHeight = constraints.maxHeight,
+                        )
+                    )
+                    layout(placeable.height, placeable.width) {
+                        placeable.place(-placeable.width, 0)
+                    }
+                }
+                .width(120.dp) // width is actually height because of the transformation
+        )
+    }
+}
