0c8d131b5aaf781d6339a2e9162f34cb121a2627
Luke <dhartman1.dh@gmail.com>
2024-08-04 22:17:19 -0400

    Add MileageViewModel functionality for copying weeks

--- app/src/main/java/com/example/mileageplanner/ui/MileageViewModel.kt
+++ app/src/main/java/com/example/mileageplanner/ui/MileageViewModel.kt
@@ -6,6 +6,9 @@ import com.example.mileageplanner.data.DayMileage
 import com.example.mileageplanner.data.MileageRepository
 import com.example.mileageplanner.utils.getWeek
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.launch
@@ -15,6 +18,23 @@ import kotlin.annotation.AnnotationTarget.CLASS
 
 class MileageViewModelImpl(private val repository: MileageRepository) : ViewModel(),
     MileageViewModel {
+
+    private val _copiedWeekMutableStateFlow = MutableStateFlow(emptyList<DayMileage>())
+    override val copiedWeekStateFlow: StateFlow<List<DayMileage>> =
+        _copiedWeekMutableStateFlow.asStateFlow()
+
+    override fun copyWeek(week: List<DayMileage>) {
+        _copiedWeekMutableStateFlow.value = week
+    }
+
+    override fun pasteWeek(week: List<LocalDate>) {
+        val newWeek = week.map { day ->
+            val newMileage = copiedWeekStateFlow.value
+                .firstOrNull { it.day.dayOfWeek == day.dayOfWeek }?.mileage ?: BigDecimal.ZERO
+            DayMileage(day, newMileage)
+        }
+        updateMileage(*newWeek.toTypedArray())
+    }
 
     override fun getAllMileageValues(): Flow<List<DayMileage>> = repository.getAllStream()
 
@@ -40,6 +60,13 @@ class MileageViewModelImpl(private val repository: MileageRepository) : ViewMode
 
 @PreviewOnly
 class MileageViewModelPreviewImpl : MileageViewModel {
+    override val copiedWeekStateFlow: StateFlow<List<DayMileage>> =
+        MutableStateFlow(emptyList<DayMileage>()).asStateFlow()
+
+    override fun copyWeek(week: List<DayMileage>) {}
+
+    override fun pasteWeek(week: List<LocalDate>) {}
+
     override fun getAllMileageValues(): Flow<List<DayMileage>> = flowOf(MileageViewModel.INITIAL)
 
     override fun getMileageValues(date: LocalDate): Flow<List<DayMileage>> =
@@ -50,15 +77,21 @@ class MileageViewModelPreviewImpl : MileageViewModel {
 
 interface MileageViewModel {
 
-    companion object {
-        val INITIAL = LocalDate.now().getWeek().map { DayMileage(it, BigDecimal.ZERO) }
-    }
+    val copiedWeekStateFlow: StateFlow<List<DayMileage>>
+
+    fun copyWeek(week: List<DayMileage>)
+
+    fun pasteWeek(week: List<LocalDate>)
 
     fun getAllMileageValues(): Flow<List<DayMileage>>
 
     fun getMileageValues(date: LocalDate): Flow<List<DayMileage>>
 
     fun updateMileage(vararg dayMileage: DayMileage)
+
+    companion object {
+        val INITIAL get() = LocalDate.now().getWeek().map { DayMileage(it, BigDecimal.ZERO) }
+    }
 }
 
 @RequiresOptIn(message = "This is to be used in Compose functions that are for Preview only.")
