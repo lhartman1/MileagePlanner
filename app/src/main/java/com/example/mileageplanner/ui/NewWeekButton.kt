3fb0ea3bcae48fc2e08eb53dcba6a03ce6a1c987
Luke <dhartman1.dh@gmail.com>
2024-08-06 18:58:03 -0400

    Add Preview

--- app/src/main/java/com/example/mileageplanner/ui/NewWeekButton.kt
+++ app/src/main/java/com/example/mileageplanner/ui/NewWeekButton.kt
@@ -3,6 +3,7 @@ package com.example.mileageplanner.ui
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
@@ -11,10 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.mileageplanner.data.DayMileage
+import com.example.mileageplanner.utils.getMonday
 import kotlinx.coroutines.launch
 import java.math.BigDecimal
 import java.time.LocalDate
+
+@OptIn(ExperimentalFoundationApi::class, PreviewOnly::class)
+@Preview(showBackground = true)
+@Composable
+private fun NewWeekButtonPreview() {
+    NewWeekButton(
+        viewModel = MileageViewModelPreviewImpl(),
+        paginatedMonday = LocalDate.now().getMonday(),
+        pagerState = rememberPagerState(pageCount = { 1 }),
+    )
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
