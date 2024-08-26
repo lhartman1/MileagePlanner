86381f060669ac4bca20c0a7f545130f3f584f5e
Luke <dhartman1.dh@gmail.com>
2024-08-25 21:54:54 -0400

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "net.lhartman.mileageplanner"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
